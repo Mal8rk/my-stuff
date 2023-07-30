4072a82d81d7ef8ec82a931cff5a8d877689f175
Mal8rk <popme2299@gmail.com>
2023-07-30 00:44:13 -0500

    working on boss level and added new music

---
+++ npc-970.lua
@@ -0,0 +1,170 @@
+local npcManager = require("npcManager")
+local npcutils = require("npcs/npcutils")
+
+local sampleNPC = {}
+
+local npcID = NPC_ID
+
+local sampleNPCSettings = {
+	id = npcID,
+
+	gfxheight = 128,
+	gfxwidth = 128,
+
+	width = 96,
+	height = 96,
+
+	gfxoffsetx = 0,
+	gfxoffsety = 0,
+
+	frames = 11,
+	framestyle = 1,
+	framespeed = 6,
+
+	speed = 1,
+
+	npcblock = false,
+	npcblocktop = false,
+	playerblock = false,
+	playerblocktop = false,
+
+	nohurt=false,
+	nogravity = false,
+	noblockcollision = false,
+	nofireball = true,
+	noiceball = true,
+	noyoshi= true,
+	nowaterphysics = false,
+
+	jumphurt = false,
+	spinjumpsafe = false,
+	harmlessgrab = false,
+	harmlessthrown = false,
+
+	grabside=false,
+	grabtop=false,
+}
+
+npcManager.setNpcSettings(sampleNPCSettings)
+
+npcManager.registerHarmTypes(npcID,
+	{
+		--HARM_TYPE_JUMP,
+		--HARM_TYPE_FROMBELOW,
+		--HARM_TYPE_NPC,
+		--HARM_TYPE_PROJECTILE_USED,
+		--HARM_TYPE_LAVA,
+		--HARM_TYPE_HELD,
+		--HARM_TYPE_TAIL,
+		--HARM_TYPE_SPINJUMP,
+		--HARM_TYPE_OFFSCREEN,
+		--HARM_TYPE_SWORD
+	}, 
+	{
+		--[HARM_TYPE_JUMP]=10,
+		--[HARM_TYPE_FROMBELOW]=10,
+		--[HARM_TYPE_NPC]=10,
+		--[HARM_TYPE_PROJECTILE_USED]=10,
+		--[HARM_TYPE_LAVA]={id=13, xoffset=0.5, xoffsetBack = 0, yoffset=1, yoffsetBack = 1.5},
+		--[HARM_TYPE_HELD]=10,
+		--[HARM_TYPE_TAIL]=10,
+		--[HARM_TYPE_SPINJUMP]=10,
+		--[HARM_TYPE_OFFSCREEN]=10,
+		--[HARM_TYPE_SWORD]=10,
+	}
+);
+
+
+
+function sampleNPC.onInitAPI()
+	npcManager.registerEvent(npcID, sampleNPC, "onTickEndNPC")
+	npcManager.registerEvent(npcID, sampleNPC, "onDrawNPC")
+end
+
+local function drawSprite(args) -- handy function to draw sprites
+	args = args or {}
+
+	args.sourceWidth  = args.sourceWidth  or args.width
+	args.sourceHeight = args.sourceHeight or args.height
+
+	if sprite == nil then
+		sprite = Sprite.box{texture = args.texture}
+	else
+		sprite.texture = args.texture
+	end
+
+	sprite.x,sprite.y = args.x,args.y
+	sprite.width,sprite.height = args.width,args.height
+
+	sprite.pivot = args.pivot or Sprite.align.TOPLEFT
+	sprite.rotation = args.rotation or 0
+
+	if args.texture ~= nil then
+		sprite.texpivot = args.texpivot or sprite.pivot or Sprite.align.TOPLEFT
+		sprite.texscale = args.texscale or vector(args.texture.width*(args.width/args.sourceWidth),args.texture.height*(args.height/args.sourceHeight))
+		sprite.texposition = args.texposition or vector(-args.sourceX*(args.width/args.sourceWidth)+((sprite.texpivot[1]*sprite.width)*((sprite.texture.width/args.sourceWidth)-1)),-args.sourceY*(args.height/args.sourceHeight)+((sprite.texpivot[2]*sprite.height)*((sprite.texture.height/args.sourceHeight)-1)))
+	end
+
+	sprite:draw{priority = args.priority,color = args.color,sceneCoords = args.sceneCoords or args.scene}
+end
+
+function sampleNPC.onTickEndNPC(v)
+	if Defines.levelFreeze then return end
+	
+	local data = v.data
+	local config = NPC.config[v.id]
+	
+	if v.despawnTimer <= 0 then
+		data.initialized = false
+		return
+	end
+
+	if not data.initialized then
+		data.initialized = true
+	end
+
+	if v:mem(0x12A, FIELD_WORD) <= 0 then
+		data.rotation = nil
+		return
+	end
+
+	if not data.rotation then
+		data.rotation = 0
+	end
+
+	if v:mem(0x12C, FIELD_WORD) > 0
+	or v:mem(0x136, FIELD_BOOL)
+	or v:mem(0x138, FIELD_WORD) > 0
+	then data.rotation = 0 return end
+
+    data.rotation = ((data.rotation or 0) + math.deg((v.speedX*config.speed)/((v.width+v.height)/4)))
+end
+
+function sampleNPC.onDrawNPC(v)
+	local config = NPC.config[v.id]
+	local data = v.data
+
+	if v:mem(0x12A,FIELD_WORD) <= 0 or not data.rotation or data.rotation == 0 then return end
+
+	local priority = -45
+	if config.priority then
+		priority = -15
+	end
+
+	drawSprite{
+		texture = Graphics.sprites.npc[v.id].img,
+
+		x = v.x+(v.width/2)+config.gfxoffsetx,y = v.y+v.height-(config.gfxheight/2)+config.gfxoffsety,
+		width = config.gfxwidth,height = config.gfxheight,
+
+		sourceX = 0,sourceY = v.animationFrame*config.gfxheight,
+		sourceWidth = config.gfxwidth,sourceHeight = config.gfxheight,
+
+		priority = priority,rotation = data.rotation,
+		pivot = Sprite.align.CENTRE,sceneCoords = true,
+	}
+
+	npcutils.hideNPC(v)
+end
+
+return sampleNPC
